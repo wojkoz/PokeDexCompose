4886a77d53f47d05befe4f3c9a39b8229700b763
Wojciech Kozioł <wkoziol3ib@gmail.com>
2022-07-24 12:42:15 +0200

    implement viewModel

--- app/src/main/java/com/example/pokecompose/presentation/poke_listings/PokeListingsViewModel.kt
+++ app/src/main/java/com/example/pokecompose/presentation/poke_listings/PokeListingsViewModel.kt
@@ -1,12 +1,63 @@
 package com.example.pokecompose.presentation.poke_listings
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pokecompose.domain.repository.PokeRepository
+import com.example.pokecompose.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PokeListingsViewModel @Inject constructor(
     private val repository: PokeRepository,
-): ViewModel(){
+) : ViewModel() {
+
+    var state by mutableStateOf(PokeListingsState())
+
+    private var searchJob: Job? = null
+
+    fun onEvent(event: PokeListingsEvent) {
+        when (event) {
+            is PokeListingsEvent.OnSearchQueryChanged -> {
+                state = state.copy(searchQuery = event.query)
+                searchJob?.cancel()
+                searchJob = viewModelScope.launch {
+                    delay(500L)
+                    getPokeListings()
+                }
+            }
+            PokeListingsEvent.Refresh -> {
+                getPokeListings(fetchFromRemote = true)
+            }
+        }
+    }
+
+    fun getPokeListings(
+        query: String = state.searchQuery.lowercase(),
+        fetchFromRemote: Boolean = false
+    ) {
+        viewModelScope.launch {
+            repository
+                .getPokeSimpleList(fetchFromRemote, query)
+                .collect { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            result.data?.let {
+                                state = state.copy(pokes = it)
+                            }
+                        }
+                        is Resource.Error -> Unit
+                        is Resource.Loading -> {
+                            state = state.copy(isLoading = result.isLoading)
+                        }
+                    }
+                }
+        }
+    }
 }
